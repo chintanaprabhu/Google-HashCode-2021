e4acafe5cc93328cc601b52a05c2d558a1db338a
anusharp97 <35512779+anusharp97@users.noreply.github.com>
2021-02-26 02:27:42 +0530

    Add files via upload

--- trafficSignalling.py
+++ trafficSignalling.py
@@ -1,4 +1,5 @@
-file = 'Input/a.txt'
+file = 'Input/c.txt'
+outputfile = 'Output/c.txt'
 with open(file, 'r') as f:
     # Reading simulation, intersections, streets, cars and points
     line = f.readline()
@@ -17,8 +18,10 @@ with open(file, 'r') as f:
     pathSet = set()
     route = [[0 for i in range(intersections)] for j in range(intersections)]
     streetToIndex = {}
+    streetToEnd = {}
     map = {}
-
+    visited = set()
+    inDegreeStreets = {}
     # Initializing route matrix
     for line in f.readlines():
         if streetCount != streets:
@@ -29,8 +32,9 @@ with open(file, 'r') as f:
             time = int(temp[3])
             route[source][dest] = 1
             streetCount += 1
-            streetToIndex[source] = street
             map[street] = [source, dest,time]
+            streetToEnd[dest] = street
+            streetToIndex[source] = street
         else:
             # Initializing paths travelled
             temp = line.split()
@@ -43,16 +47,56 @@ with open(file, 'r') as f:
             carCount += 1
     print(route)
     print(paths)
-    print(pathSet)
     print(streetToIndex)
     print(map)
-    # Initializing inDegrees
-    # for i in pathSet:
-    #     for j in paths:
-    #         if i in j:
-    #             if i in inDegrees:
-    #                 inDegrees[i] += 1
-    #             else:
-    #                 inDegrees[i] = 1
-    # print(inDegrees)
+    matrixTranspose = [list(i) for i in zip(*route)]
+    n = len(matrixTranspose)
+    for i in range(n):
+        row = matrixTranspose[i]
+        inDegreeStreets[i] = []
+        if row.count(1)>1:
+           if i in inDegrees:
+               inDegrees[i] += 1
+           else:
+               inDegrees[i] = 2
+           length = len(row)
+           for j in range(length):
+               r = row[j]
+               #print('here', r)
+               if r==1:
+                   inDegreeStreets[i].append(streetToIndex[j])
+                   #print('here',j)
+
+    print(inDegrees)
+    print(streetToEnd)
+    lightsCount = len(inDegrees)
+    text = ""
+    with open(outputfile, 'w') as f:
+        for path in paths:
+            for p in path[1:]:
+                if map[p][0] not in inDegrees and map[p][0] not in visited:
+                    text += str(map[p][0])+"\n"+"1"+"\n"+streetToEnd[map[p][0]]+" "+"1"+"\n"
+                    visited.add(map[p][0])
+                    #print(p, map[p][0])
+                    lightsCount += 1
+        for key,value in inDegrees.items():
+            text+=str(key)+"\n"+str(value)+"\n"
+            roads = inDegreeStreets[key]
+            val = 1
+            flag = 0
+            for r in roads:
+                if not flag:
+                    val = 1
+                else:
+                    val = 2
+                text+= r+" "+str(val)+"\n"
+                flag = not flag
+                #print(roads)
+
+        print(lightsCount)
+        print(text)
+        f.write(str(lightsCount)+"\n")
+        f.write(text)
+
+
 
